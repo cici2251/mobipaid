77634395bc615a4b920b66c88e2ee1bf3ba63269
cici2251 <iljamiatin02@gmail.com>
2023-05-15 11:04:14 +0700

    system mobipaid register and login

---
+++ Object Repository/Page_Register/select_Your country                        _da403c.rs
@@ -0,0 +1,197 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>select_Your country                        _da403c</name>
+   <tag></tag>
+   <elementGuidId>9921be31-b0d5-4328-aebc-be740b30e1b4</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#country</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//select[@id='country']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>select</value>
+      <webElementGuid>699ea39b-e834-4f87-a1ba-cc8ee5be5509</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>region</value>
+      <webElementGuid>eb95dd05-2b02-429f-abe0-9efa97b13aab</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>form-control border-grey</value>
+      <webElementGuid>47107a90-c093-4f21-b695-99ea70a2359a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>country</value>
+      <webElementGuid>38196614-6fa1-486e-a830-69c9e624ed1e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>onchange</name>
+      <type>Main</type>
+      <value>handle_state_change()</value>
+      <webElementGuid>e37d7539-2fa8-418a-8bad-68fecaec1bf2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                  Your country
+                                       
+                      South Africa                    
+                                       
+                      South Africa nedbank                    
+                                       
+                      United states                    
+                                       
+                      irlandia                    
+                                       
+                      Europe                    
+                                       
+                      Indonesia                    
+                                       
+                      Japan                    
+                                       
+                      Wakanda                    
+                                       
+                      testnew                    
+                                       
+                      test123                    
+                                  </value>
+      <webElementGuid>6a1240b1-db42-4a2e-9788-93b8a84d4576</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;country&quot;)</value>
+      <webElementGuid>5606746e-32b2-419e-b1c8-3c63c7f06fbb</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//select[@id='country']</value>
+      <webElementGuid>c8d6892d-3e0e-4938-91f1-83211f589a0e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//form[@id='formRegister']/div[5]/div[3]/select</value>
+      <webElementGuid>57f5bc01-4c27-4931-b4ab-1596ef7c8100</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Åland Islands'])[1]/following::select[1]</value>
+      <webElementGuid>dd64232d-93ac-4779-9f0c-2841048c9a88</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Zimbabwe'])[1]/following::select[1]</value>
+      <webElementGuid>b0fcd552-a254-40d6-8782-f8769c063425</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Payment Services Agreement'])[1]/preceding::select[2]</value>
+      <webElementGuid>55214cc6-4e64-475e-bc54-9878160f3503</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[5]/div[3]/select</value>
+      <webElementGuid>96514bc8-c66b-4663-a737-99730f41ad5e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//select[@name = 'region' and @id = 'country' and (text() = '
+                  Your country
+                                       
+                      South Africa                    
+                                       
+                      South Africa nedbank                    
+                                       
+                      United states                    
+                                       
+                      irlandia                    
+                                       
+                      Europe                    
+                                       
+                      Indonesia                    
+                                       
+                      Japan                    
+                                       
+                      Wakanda                    
+                                       
+                      testnew                    
+                                       
+                      test123                    
+                                  ' or . = '
+                  Your country
+                                       
+                      South Africa                    
+                                       
+                      South Africa nedbank                    
+                                       
+                      United states                    
+                                       
+                      irlandia                    
+                                       
+                      Europe                    
+                                       
+                      Indonesia                    
+                                       
+                      Japan                    
+                                       
+                      Wakanda                    
+                                       
+                      testnew                    
+                                       
+                      test123                    
+                                  ')]</value>
+      <webElementGuid>fd5591af-fc28-41a2-b823-93a495223573</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
